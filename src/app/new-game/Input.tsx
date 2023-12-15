3840a9181a73e467d8bebdec5b9c2d1a9d61fb8a
Piszczek <ognisty001@gmail.com>
2023-12-15 02:02:44 +0100

    Players count added

--- src/app/new-game/Input.tsx
+++ src/app/new-game/Input.tsx
@@ -1,22 +1,112 @@
-export default function Input({
-  field,
-  type,
-}: {
-  field: string;
-  type: string;
-}) {
+import { ChangeEvent, useEffect, useState } from "react";
+
+export default function Input({ count }: { count: number }) {
+  const [militaryInput, setMilitaryInput] = useState(0);
+  const [moneyInput, setMoneyInput] = useState(0);
+  const [wonderInput, setWonderInput] = useState(0);
+  const [blueInput, setBlueInput] = useState(0);
+  const [yellowInput, setYellowInput] = useState(0);
+  const [scienceInput, setScienceInput] = useState(0);
+  const [guildInput, setGuildInput] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleInputChange =
+    (inputSetter: React.Dispatch<React.SetStateAction<number>>) =>
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const value = parseInt(e.target.value);
+      inputSetter(isNaN(value) ? 0 : value);
+    };
+
+  useEffect(() => {
+    setSum(
+      militaryInput +
+        moneyInput +
+        wonderInput +
+        blueInput +
+        yellowInput +
+        scienceInput +
+        guildInput
+    );
+  }, [
+    militaryInput,
+    moneyInput,
+    wonderInput,
+    blueInput,
+    yellowInput,
+    scienceInput,
+    guildInput,
+  ]);
+
   return (
     <>
-      {[1, 2, 3, 4, 5, 6, 7].map((num) => (
-        <input
-          key={num}
-          id={`${field}${num}`}
-          type={type}
-          name={`${field}${num}`}
-          autoComplete="false"
-          className="border-l-2 border-black"
-        />
-      ))}
+      <input
+        type="text"
+        name={`player${count}`}
+        autoComplete="off"
+        className="bg-slate-700 border-l-2 border-black text-slate-200"
+      />
+      <input
+        list="cities"
+        name={`city${count}`}
+        className="bg-slate-400 border-l-2 border-black"
+      />
+      <input
+        type="number"
+        name={`military${count}`}
+        autoComplete="off"
+        className="bg-red-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setMilitaryInput)}
+      />
+      <input
+        type="number"
+        name={`money${count}`}
+        autoComplete="off"
+        className="bg-amber-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setMoneyInput)}
+      />
+      <input
+        type="number"
+        name={`wonder${count}`}
+        autoComplete="off"
+        className="bg-cyan-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setWonderInput)}
+      />
+      <input
+        type="number"
+        name={`blue${count}`}
+        autoComplete="off"
+        className="bg-blue-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setBlueInput)}
+      />
+      <input
+        type="number"
+        name={`yellow${count}`}
+        autoComplete="off"
+        className="bg-yellow-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setYellowInput)}
+      />
+      <input
+        type="number"
+        name={`science${count}`}
+        autoComplete="off"
+        className="bg-emerald-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setScienceInput)}
+      />
+      <input
+        type="text"
+        name={`guild${count}`}
+        autoComplete="off"
+        className="bg-violet-400 border-l-2 border-black text-center"
+        onChange={handleInputChange(setGuildInput)}
+      />
+      <input
+        type="number"
+        name={`sum${count}`}
+        autoComplete="off"
+        value={sum}
+        className="bg-slate-800 border-l-2 border-black text-slate-200 text-center"
+        disabled
+      />
     </>
   );
 }
--- src/app/new-game/page.tsx
+++ src/app/new-game/page.tsx
@@ -1,55 +1,82 @@
 "use client";
-import InputCity from "./InputCity";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import Input from "./Input";
+import { useState } from "react";
 
 export default function Page() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = searchParams.get("count");
+  const [count, setCount] = useState(7);
+
   return (
     <main>
       <div className="max-w-lg mx-auto">
-        <form action={"/api/add"} method="POST" className="flex flex-col">
-          <div className="flex flex-col rounded-lg overflow-hidden">
-            <div className="grid grid-cols-8 bg-slate-700 text-white">
-              <p className="bg-slate-800 p-1">Nazwa</p>
-              <Input field="player" type="text" />
+        {params ? (
+          <form action="/api/add" method="POST" className="flex flex-col">
+            <div className="grid grid-rows-[repeat(10,minmax(0,1fr))] grid-flow-col">
+              <p className="bg-slate-800 p-1 text-slate-200">Player</p>
+              <p className="bg-slate-500 p-1">City</p>
+              <p className="bg-red-500 p-1">Military</p>
+              <p className="bg-amber-500 p-1">Money</p>
+              <p className="bg-cyan-500 p-1">Wonder</p>
+              <p className="bg-blue-500 p-1">Blue</p>
+              <p className="bg-yellow-500 p-1">Yellow</p>
+              <p className="bg-emerald-500 p-1">Science</p>
+              <p className="bg-violet-500 p-1">Guild</p>
+              <p className="bg-black p-1 text-slate-200">Sum</p>
+              {[...Array(count)].map((_, num) => (
+                <Input key={num} count={num} />
+              ))}
             </div>
-            <div className="grid grid-cols-8 bg-slate-300">
-              <p className="bg-slate-400 p-1">Miasto</p>
-              <InputCity />
+            <button
+              type="submit"
+              className="bg-blue-500 rounded-lg mt-1 p-1 font-bold text-slate-200"
+            >
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={() => router.push(pathname)}
+              className="bg-blue-500 rounded-lg mt-1 p-1 font-bold text-slate-200"
+            >
+              Change players number
+            </button>
+          </form>
+        ) : (
+          <div className="flex flex-col">
+            <div className="flex">
+              <label htmlFor="count" className="text-lg font-bold p-1">
+                How many players:
+              </label>
+              <input
+                type="number"
+                name="count"
+                className="w-1/2 grow bg-sky-200 rounded-lg border-2 border-black p-1"
+                onChange={(e) => setCount(parseInt(e.target.value))}
+              />
             </div>
-            <div className="grid grid-cols-8 bg-red-300">
-              <p className="bg-red-400 p-1">Wojna</p>
-              <Input field="military" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-yellow-300">
-              <p className="bg-yellow-400 p-1">Złoto</p>
-              <Input field="money" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-orange-300">
-              <p className="bg-orange-400 p-1">Cuda</p>
-              <Input field="wonder" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-blue-300">
-              <p className="bg-blue-400 p-1">Cywilne</p>
-              <Input field="blue" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-yellow-300">
-              <p className="bg-yellow-400 p-1">Żółte</p>
-              <Input field="yellow" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-green-300">
-              <p className="bg-green-400 p-1">Nauka</p>
-              <Input field="science" type="number" />
-            </div>
-            <div className="grid grid-cols-8 bg-violet-300">
-              <p className="bg-violet-400 p-1">Gildia</p>
-              <Input field="guild" type="number" />
-            </div>
+            <button
+              type="button"
+              onClick={() => router.push(`${pathname}?count=${count}`)}
+              className="bg-blue-500 rounded-lg mt-1 p-1 font-bold text-slate-200"
+            >
+              Create
+            </button>
           </div>
-          <button type="submit" className="bg-blue-500 rounded-lg mt-1">
-            Submit
-          </button>
-        </form>
+        )}
       </div>
+
+      <datalist id="cities">
+        <option value="Giza" />
+        <option value="Rhodes" />
+        <option value="Babylon" />
+        <option value="Alexandria" />
+        <option value="Halicarnassus" />
+        <option value="Olympia" />
+        <option value="Ephesus" />
+      </datalist>
     </main>
   );
 }
